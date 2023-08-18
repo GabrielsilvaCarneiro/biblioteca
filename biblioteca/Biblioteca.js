673a4111e1b6d9edbec84e04ab62d13bbaa11be7
Gabriel Carneiro <133365361+GabrielsilvaCarneiro@users.noreply.github.com>
2023-08-18 15:54:37 -0300

    Biblioteca

---
+++ biblioteca/Biblioteca.js
@@ -0,0 +1,54 @@
+// let biblioteca = [];
+
+// function cadastro(nome, autor, ano){
+//     var livro = {
+//         titulo: nome,
+//         autor: autor,
+//         ano: ano
+//     };
+//     biblioteca.push(livro)
+// }
+
+// function adicionar (){
+//     let nome = document.getElementById('nomeLivro').value;
+//     let autor = document.getElementById('autorLivro').value;
+//     let ano = document.getElementById('anoLivro').value;
+//     cadastro(nome, autor, ano); 
+// }
+// function mostrarLivros (){
+//     console.log(`resultado:${biblioteca[0].titulo}`);
+// };
+
+// let imgIndice = 0;
+// const IMAGENS =['assim que acaba.png','sangue e cinzas.png', 'princesa de cinzas.png'];
+// const PrevButton = document.getElementById('anterior');
+// const NextButton = document.getElementById('proximo');
+
+// var IMG = document.getElementById('carroselimagem')
+
+// function carregar() {
+// IMG.src = IMAGENS[imgIndice]
+// }
+
+// prevButton.addEventListener('click', () => {
+//     if (imgIndice > 0 ) {
+//     --imgIndice
+//     //imgIndice = imgIndice - 1;
+//     carregar();
+//     }
+// });
+
+// nextButton.addEventListener('click',() => {
+//     var limite=(IMAGENS.length)-1
+//     if (imgIndice < limite) {
+//         ++imgIndice
+//         //imgIndice = imgIndice +1;
+//         carregar();
+//     }
+// })
+// carregar();
+
+//     carregar();
+
+
+
